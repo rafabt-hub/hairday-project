56bd957509b2ba326d9b12da1efda221ed46c0e9
Rafael <rafaelbthomaz@gmail.com>
2025-02-21 15:37:45 -0300

    adding hours on js

---
+++ src/modules/schedules/load.js
@@ -0,0 +1,10 @@
+import { hoursLoad } from "../form/hours-load.js"
+
+const selectedDate = document.getElementById("date")
+
+export function schedulesDay() {
+
+  const date = selectedDate.value
+
+  hoursLoad({ date })
+}
